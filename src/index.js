d716810fd4c55da65ac024744e57de7881bfb2ce
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-03-19 20:41:44 +0200

    gh-pages

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import { theme } from "./theme/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/genesis/">
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </BrowserRouter>
-  // </React.StrictMode>
 );
