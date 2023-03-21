49b46fd74eaa5ab27caf8ae2807a8cbdeb8d0fde
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-03-21 15:03:53 +0200

    change basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { theme } from "./theme/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
